2de29a6a023dc069ee1c8d7e0919b085d2b64ca9
Arstanovich <bek64418@gmail.com>
2023-06-08 23:24:07 +0300

    flash chat

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,14 +1,19 @@
 import 'package:flash_chat/modules/landing/view/landing_view.dart';
+import 'package:flash_chat/modules/sigUp/views/sign_up_view.dart';
 import 'package:get/get.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
-  static const INITTAL = Routes.LANDING;
+  static const INITTAL = Routes.SIGNUP;
   static final routes = [
     GetPage(
       name: _Paths.LANDING, page: () => const LandingView(),
+      // binding: LandingB(),
+    ),
+    GetPage(
+      name: _Paths.SIGNUP, page: () => const SignUpView(),
       // binding: LandingB(),
     ),
   ];
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -3,11 +3,11 @@ part of "app_pages.dart";
 abstract class Routes {
   Routes._();
   static const LANDING = _Paths.LANDING;
-  // static const SIGNUP = _Paths.SIGNUP;
+  static const SIGNUP = _Paths.SIGNUP;
 }
 
 abstract class _Paths {
   _Paths._();
   static const LANDING = "/landing";
-  // static const SIGNUP = '/signup';
+  static const SIGNUP = '/signup';
 }
